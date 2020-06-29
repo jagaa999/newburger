e6185a07ce45293f37e15b24477efabfec47c027
jagaa <jargal@kt.mn>
2020-06-29 16:31:15 +0800

    Updates

---
+++ precache-manifest.a08bf0320bd60298c5beb12496dd3241.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4903ce3085fe0a1c3b4d2a2e823802d7",
+    "url": "/newburger/index.html"
+  },
+  {
+    "revision": "abc3187da217237ab6bf",
+    "url": "/newburger/static/css/main.d5449b0f.chunk.css"
+  },
+  {
+    "revision": "d8d5d540e5ce9e07cec4",
+    "url": "/newburger/static/js/2.76377108.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/newburger/static/js/2.76377108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc3187da217237ab6bf",
+    "url": "/newburger/static/js/main.e1898817.chunk.js"
+  },
+  {
+    "revision": "c3c228fa6c9b24e245f9",
+    "url": "/newburger/static/js/runtime-main.e1846826.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/newburger/static/media/burger-logo.b8503d26.png"
+  }
+]);
